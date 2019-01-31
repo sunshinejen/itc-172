ad19e4c7165095c842182b1a3a550ae7639f1389
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2019-01-30 19:17:05 -0800

    assignment 5 uploads
    changes to urls, views, base.html and adds types.html

--- club/views.py
+++ club/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Resource
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+def resourcetype (request):
+    resource_list= Resource.objects.all()
+    return render(request, 'club/types.html', {'resource_list': resource_list})
