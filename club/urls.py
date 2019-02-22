7d358b8cd8b71ca1a36332e2df38fe7b2552f638
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2019-02-21 17:19:21 -0800

    Assignment 6
    Assignment6 Meeting and detail

--- club/urls.py
+++ club/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('resourcetype/', views.resourcetype, name='resourcetype')
+    path('resourcetype/', views.resourcetype, name='resourcetype'),
+    path('getmeeting/', views.getmeeting, name='getmeeting'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='details'),
+
 ]
