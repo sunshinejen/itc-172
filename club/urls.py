5251234e40dcc50d4ede6a6cec85802634f200a7
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2019-02-28 15:23:10 -0800

    Assignment 9 authentication
    Adds login view
    logout view
    login form
    modify the form view to require authentication
    add link to login form to base.html

--- club/urls.py
+++ club/urls.py
@@ -7,4 +7,7 @@ urlpatterns=[
     path('getmeeting/', views.getmeeting, name='getmeeting'),
     path('meetingdetail/<int:id>', views.meetingdetail, name='details'),
     path('newMeeting/', views.newMeeting, name ='newMeeting'),
+    path('newResource/', views.newResource, name ='newResource'),
+    path('loginmessage/', views.loginmessage, name = 'loginmessage'),
+    path('logoutmessage/', views.logoutmessage, name = 'logoutmessage'),
 ]
